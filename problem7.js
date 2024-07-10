10be043d3f87878a71d4c6cb62d5c6d8af2c83c3
Muhammad Shakir <chaudhryshakir67@gmail.com>
2024-07-10 14:10:17 +0500

    Update problem7.js

--- problem7.js
+++ problem7.js
@@ -1,1 +1,14 @@
+// Write a program that prints the first n terms of the Fibonacci sequence, where n is a user input.
 
+function fabonacciSequence(n) { 
+    let a = 0, b = 1, nextTerm;
+
+    for (let i = 1; i <= n; i++) { 
+        console.log(a);
+        nextTerm = a + b;
+        a = b;
+        b = nextTerm;
+    }
+}
+
+fabonacciSequence(5)
