315f00619134540a57bcc64b246edb0a44aab52c
Muhammad Shakir <chaudhryshakir67@gmail.com>
2024-07-10 14:08:16 +0500

    Update problem2.js

--- problem2.js
+++ problem2.js
@@ -1,1 +1,12 @@
+// Write a program that calculates the sum of the first n natural numbers, where n is a user input. 
 
+function sumOfNaturalNums(n) { 
+    let sum = 0;
+    for (let i = 1; i <= n; i++) { 
+        sum += i
+    }
+
+    return sum;
+}
+
+console.log(sumOfNaturalNums(5))
