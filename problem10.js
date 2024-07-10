4af4f506ad41493d4a213cdb6fe216640f4d5e82
Muhammad Shakir <chaudhryshakir67@gmail.com>
2024-07-10 14:10:43 +0500

    Update problem10.js

--- problem10.js
+++ problem10.js
@@ -1,1 +1,17 @@
+// Print Even Numbers in an Array
 
+let arr = [10, 20, 67, 78, 90, 45, 22, 44];
+
+function printEvenNums(arr) { 
+
+    let evenNums = [];
+    for (let i = 0; i <= arr.length; i++) { 
+        if (arr[i] % 2 === 0) { 
+            evenNums.push(arr[i])
+        }
+    }
+
+    return evenNums;
+}
+
+console.log(printEvenNums(arr))
