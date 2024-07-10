c5e056746d1f836af48c28df6361bf7ab06b19da
Muhammad Shakir <chaudhryshakir67@gmail.com>
2024-07-10 14:08:52 +0500

    Update problem4.js

--- problem4.js
+++ problem4.js
@@ -1,1 +1,9 @@
+// Write a program that prints the multiplication table of a given number n.
 
+function printTable(n) { 
+    for (let i = 1; i <= 10; i++) { 
+        console.log(`${n} * ${i} = ${n*i}`)
+    }
+} 
+
+printTable(2)
