161cbcb21866b2b6fd57a7238f7017a7afb211cf
Muhammad Shakir <chaudhryshakir67@gmail.com>
2024-07-10 14:09:21 +0500

    Update problem5.js

--- problem5.js
+++ problem5.js
@@ -1,1 +1,24 @@
+// Write a program that reverses a given string using a for loop.
+// Now there are many ways to reverse a string.
 
+// method -1
+
+function reverseString(str) { 
+     return str.split("").reverse().join("")
+}
+
+console.log(reverseString("Shakir"))
+
+// method - 2 using for loop
+
+function reverseString(str) { 
+    let newStr = "";
+    
+    for (let i = str.length - 1; i >= 0; i--) { 
+        newStr += str[i] 
+    }
+
+    return newStr
+}
+
+console.log(reverseString("Shakir"));
