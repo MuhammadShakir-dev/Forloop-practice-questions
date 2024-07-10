cdd12abd290ca2eda05b1ebaad0f0d8d6938acc5
Muhammad Shakir <chaudhryshakir67@gmail.com>
2024-07-10 16:20:57 +0500

    Update problem9.js

--- problem9.js
+++ problem9.js
@@ -1,1 +1,14 @@
+function isPrime(num) {
+    if (num <= 1) {
+        return false;
+    }
+
+    for (let i = 2; i <= Math.sqrt(num); i++) {
+        if (num % i === 0) {
+            return false;
+        }
+    }
+
+    return true;
+}
 
