db9cca33ef7bbfe07383474e6056a9f3c1e65ef1
Muhammad Shakir <chaudhryshakir67@gmail.com>
2024-07-10 14:08:36 +0500

    Update problem3.js

--- problem3.js
+++ problem3.js
@@ -1,1 +1,12 @@
+// Write a program that calculates the factorial of a given number n.
 
+let num = 5;
+function findFactoiral(num) { 
+    let factorial = 1;
+
+    for (let i = 1; i <= num; i++) { 
+        factorial *= i
+    }
+
+    return factorial
+}
